c8db82ec2f4cf80d6e74a1a33c9f14f0e33e267e
allecosta <alle_costa10@outlook.com>
2024-01-04 20:48:04 -0300

    refactor: release v0.1

--- admin/users_photo.php
+++ admin/users_photo.php
@@ -1,30 +1,29 @@
 <?php
-	include 'includes/session.php';
 
-	if(isset($_POST['upload'])){
-		$id = $_POST['id'];
-		$filename = $_FILES['photo']['name'];
-		if(!empty($filename)){
-			move_uploaded_file($_FILES['photo']['tmp_name'], '../images/'.$filename);	
-		}
-		
-		$conn = $pdo->open();
+require 'includes/session.php';
 
-		try{
-			$stmt = $conn->prepare("UPDATE users SET photo=:photo WHERE id=:id");
-			$stmt->execute(['photo'=>$filename, 'id'=>$id]);
-			$_SESSION['success'] = 'User photo updated successfully';
-		}
-		catch(PDOException $e){
-			$_SESSION['error'] = $e->getMessage();
-		}
+if (isset($_POST['upload'])) {
+	$id = $_POST['id'];
+	$filename = $_FILES['photo']['name'];
 
-		$pdo->close();
-
-	}
-	else{
-		$_SESSION['error'] = 'Select user to update photo first';
+	if (!empty($filename)) {
+		move_uploaded_file($_FILES['photo']['tmp_name'], '../images/' . $filename);
 	}
 
-	header('location: users.php');
-?>
+	$conn = $pdo->open();
+
+	try {
+		$sql = "UPDATE users SET photo = :photo WHERE id = :id";
+		$stmt = $conn->prepare($sql);
+		$stmt->execute(['photo' => $filename, 'id' => $id]);
+		$_SESSION['success'] = 'Foto do usuário atualizada com sucesso.';
+	} catch (PDOException $e) {
+		$_SESSION['error'] = $e->getMessage();
+	}
+
+	$pdo->close();
+} else {
+	$_SESSION['error'] = 'Favor, primeiro selecione a foto do usuário a ser atualizado.';
+}
+
+header('location: users.php');
